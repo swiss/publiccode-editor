9aa7fcd0cfeecf59107f89ec1fd207289fb11a68
Claudia Asti <asti@puzzle.ch>
2025-11-05 13:16:27 +0100

    feat: set italian country extension default to undefined

--- src/app/components/Editor.tsx
+++ src/app/components/Editor.tsx
@@ -26,7 +26,6 @@ import maintenanceTypes from "../contents/maintenanceTypes";
 import mimeTypes from "../contents/mime-types";
 import platforms from "../contents/platforms";
 import PublicCode, {
-  defaultItaly,
   IT_COUNTRY_EXTENSION_VERSION,
   LATEST_VERSION,
   PublicCodeWithDeprecatedFields,
@@ -141,7 +140,7 @@ const defaultValues = {
   platforms: [],
   categories: undefined,
   description: {},
-  it: defaultItaly,
+  it: undefined
 };
 
 const isNotTheSameVersion = (version1: string, version2: string) => {
